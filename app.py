63c6cc1b5f9236ef4cad803fc53017c04bfdcf58
su-learning-labo <groovin_high@i-iide.com>
2023-11-14 01:32:24 +0900

    add text analyze and wordcloud

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import seaborn as sns
 import japanize_matplotlib
 from wordcloud import WordCloud
 from collections import Counter
+import json
 
 if st.secrets.ENVIRONMENT == 'production':
     base_url = 'https://articles-sentiment-app-back.onrender.com'
@@ -41,7 +42,7 @@ def get_data():
     sentiment = get_sentiment_data()
     data = pd.merge(article, sentiment, left_on='id', right_on='article_id')
     df = data.filter([
-        'title', 'description', 'url', 'published_at', 'fetched_at', 'ranking', 'sentiment', 'score'
+        'title', 'description', 'published_at', 'fetched_at', 'sentiment', 'score'
     ])
     return df
 
@@ -66,11 +67,19 @@ def morphological(text):
         return response.json()
 
 
+def analyze_nouns(text):
+    url = base_url + '/extract-nouns/'
+    response = requests.post(url, json={"text": text})
+
+    if response.status_code == 200:
+        return response.json()
+
+
 # WordCloud
 def generate_wordcloud(text):
     wordcloud = WordCloud(
         background_color='white',
-        font_path='static/Yusei_Magic/YuseiMagic-Regular.ttf',
+        font_path='static/Sawarabi_Gothic/SawarabiGothic-Regular.ttf',
         width=800, height=400
     ).fit_words(text)
     fig, ax = plt.subplots(figsize=(10, 5))
@@ -102,43 +111,73 @@ def main():
     header_left, header_mid, header_right = st.columns([2.5, 2.5, 5])
     date_from = header_left.date_input('いつから', min_date)
     date_to = header_mid.date_input('いつまで')
-    st.write('')
+    under_head_left, under_head_right = st.columns([5, 5])
 
-    st.write('条件設定が完了したら下の表示ボタンを押してください')
-    st.write('')
+    if 'button_clicked' not in st.session_state:
+        st.session_state['button_clicked'] = False
 
-    if st.button('分析開始'):
+    if under_head_left.button('データを表示', use_container_width=True):
+        st.session_state['button_clicked'] = True
+
+    if st.session_state['button_clicked']:
         # データの取得
-        df = get_data().filter(['title', 'description', 'published_at', 'fetched_at', 'sentiment', 'score', 'ranking'])
+        st.write('')
+        st.write('-- ニュース記事  from NewsAPI --')
+        df = get_data().filter(['title', 'description', 'published_at', 'fetched_at', 'sentiment', 'score'])
 
         df['published_at'] = pd.to_datetime(df['published_at']).dt.date
         df['fetched_at'] = pd.to_datetime(df['fetched_at']).dt.date
         df.set_index('published_at', inplace=True)
-        output = df.query('@date_from <= fetched_at <= @date_to')
+        output = df.query('@date_from <= published_at <= @date_to')
+        output['title'] = output['title'].str.split('-').str[0].str.split('|').str[0]
+        st.dataframe(output)
 
-        st.write(output)
+        st.write('')
+        st.write('条件設定が完了したら下の表示ボタンを押してください')
 
-        # WordCloudの前処理
-        text = list(output['title'])
-        text = ','.join(text)
-        # st.write(text)
-        nouns = morphological(text)
+        if st.button('分析開始'):
 
-        # 除外するキーワード指定
-        exclude_words = ['年', '月', '日', '|', 'com', 'Powered', 'by', 'ｋｍ', '東スポ', '読売', '.co.jp', 'NEWS', 'さん', 'JBpress', '%', '％', 'さま','位', '新聞', '毎日']
-        output_text = []
-        for noun in nouns['nouns']:
-            if noun not in exclude_words:
-                output_text.append(noun)
+            # テキスト抽出
+            text = list(output['title'])
+            text = ','.join(text)[:500]
 
-        words_count = Counter(output_text)
-        result = words_count.most_common()[:30]
-        st.write(pd.DataFrame(result))
-        generate_wordcloud(dict(result))
+            # グラフ（割合）
 
-        # fig, ax = plt.subplots()
-        # sns.violinplot(data=df, x='fetched_at', y='score', hue='sentiment')
-        # st.pyplot(fig)
+            con_body = st.container()
+            con_body.write('## 分析結果')
+            body_left, body_right = con_body.columns([3, 2])
+            body_left.caption('<原文>')
+            body_left.write(text)
+
+            # 形態素解析による分類
+            body_right.caption('<形態素解析による分類>')
+            analyze_text = morphological(text)
+            df = pd.DataFrame(analyze_text['tokens'])
+            df.set_index('token_no', inplace=True)
+            body_right.dataframe(df)
+
+            nouns = analyze_nouns(text)
+
+            # 除外するキーワード指定
+            exclude_words = [
+                '年', '月', '日', 'ｋｍ', 'さん', '%', '％', 'さま', '位'
+            ]
+            output_text = []
+            for noun in nouns['nouns']:
+                if noun not in exclude_words:
+                    output_text.append(noun)
+
+            words_count = Counter(output_text)
+            result = words_count.most_common()
+            # st.write(pd.DataFrame(result))
+
+            con_body.write('---')
+            con_body.caption('<ワードクラウドによる可視化>')
+            generate_wordcloud(dict(result))
+
+            # fig, ax = plt.subplots()
+            # sns.violinplot(data=df, x='fetched_at', y='score', hue='sentiment')
+            # st.pyplot(fig)
 
 
 if __name__ == "__main__":
